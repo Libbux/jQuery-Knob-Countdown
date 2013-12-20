cf7d97e0b33be320968e875c48e402f21fc1f6e5
Jonathan Libby <j@thelibbster.com>
2013-12-19 20:26:25 -0500

    Patch for post-event counter restarting
    Before this update, the countdown timer would reset itself to exactly 24 hours, the second the event date passed to it. After this update, the timer will simply sit at 0 once the event it is counting down until has passed.

--- js/countdown.js
+++ js/countdown.js
@@ -6,21 +6,26 @@
 		}
 
 		this_sel = $(this);
-
+		
 		function count_exec() {
-			eventDate = Date.parse(settings['date']) / 1000;	// Parse the date string
-			currentDate = 	Math.floor($.now() / 1000);					// Find the timestamp for now
-			seconds = eventDate - currentDate;					// Find the number of seconds remaining
-
-			days = 			Math.floor(seconds / (60 * 60 * 24));		// Divide to find the number of days remaining
-			seconds -=		days * 60 * 60 * 24;						// Subtract the number of (complete, => 24 hours) days calculated above
-			
-			hours = 		Math.floor(seconds / (60 * 60));			// Get the number of hours from that modified number ^
-			seconds -=		hours * 60 * 60;
-			
-			minutes = 		Math.floor(seconds / 60);
-			seconds -=		minutes * 60;
-
+			eventDate = Date.parse(settings['date']) / 1000; // Parse the date string
+			currentDate = 	Math.floor($.now() / 1000); // Find the timestamp for now
+			seconds = eventDate - currentDate; // Find the number of seconds remaining
+			if (seconds <= 0) { // After the event date has passed
+				days = 0;
+				hours = 0;
+				minutes = 0;
+				seconds = 0;
+			} else {
+				days = 			Math.floor(seconds / (60 * 60 * 24));		// Divide to find the number of days remaining
+				seconds -=		days * 60 * 60 * 24;						// Subtract the number of (complete, => 24 hours) days calculated above
+				
+				hours = 		Math.floor(seconds / (60 * 60));			// Get the number of hours from that modified number ^
+				seconds -=		hours * 60 * 60;
+				
+				minutes = 		Math.floor(seconds / 60);
+				seconds -=		minutes * 60;
+			}
 			this_sel.find('#days').val(days).trigger('change');
 			this_sel.find('#hours').val(hours).trigger('change');
 			this_sel.find('#mins').val(minutes).trigger('change');
